7c11145f4701e6df934fc5cd125dc02ac3201df5
yashG <yrg.creation.ai@gmail.com>
2025-12-14 20:46:12 +0530

    append child

--- script.js
+++ script.js
@@ -1,5 +1,28 @@
-const centerDiv = document.querySelector(".center")
+// concept of throttling - reduce the number of function executions
 
-centerDiv.addEventListener("mousemove", (para) => {
-    console.log(para.clientX, para.clientY)
-})
+const throttleFunction = (func, delay) => {
+    let prev = 0;
+    return(...args) => {
+        let now = new Date().getTime();
+        if (now - prev > delay) {
+            prev = now;
+            return func(...args);
+        }
+    };
+};
+
+const mainDiv = document.querySelector(".main")
+// throttleFunction will be run when mousemove on centerDiv in between the delay of 400 
+const centerDiv = document.querySelector(".center");
+centerDiv.addEventListener("mousemove", throttleFunction((el) => {
+// less repeated code (the element we want to create on mouse move)
+   var div = document.createElement("div");
+   div.classList.add("card")
+
+   div.style.left = el.clientX +'px';
+   div.style.top = el.clientY +'px';
+
+   mainDiv.appendChild(div)
+
+}, 400)
+);
